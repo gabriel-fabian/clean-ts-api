8d00e9d1863bb0fab1ee163f6c65d9948b73ebbc
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-04-07 21:50:06 -0300

    feat: ensure EmailValidatorAdapter returns false if validator returns false

---
+++ src/utils/email-validator.ts
@@ -0,0 +1,7 @@
+import { EmailValidator } from '../presentation/protocols/email-validator'
+
+export class EmailValidatorAdapter implements EmailValidator {
+  isValid (_email: string): boolean {
+    return false
+  }
+}
