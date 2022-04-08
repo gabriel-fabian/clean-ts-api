f219339f5c13f7d11c7110ec12a07542be009a85
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-04-07 23:07:47 -0300

    style: remove space before function

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -4,7 +4,7 @@ import { SignUpController } from './signup'
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
-    isValid (_email: string): boolean {
+    isValid(_email: string): boolean {
       return true
     }
   }
@@ -13,7 +13,7 @@ const makeEmailValidator = (): EmailValidator => {
 
 const makeAddAccount = (): AddAccount => {
   class AddAccountStub implements AddAccount {
-    async add (_account: AddAccountModel): Promise<AccountModel> {
+    async add(_account: AddAccountModel): Promise<AccountModel> {
       const fakeAccount = {
         id: 'valid_id',
         name: 'valid_name',
