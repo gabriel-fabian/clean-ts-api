d431c2362dbb5966ddd9a190baf8c14e8b5f2608
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-04-11 19:50:23 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,11 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AccountModel } from '../../../../domain/models/account'
+import { AddAccountModel } from '../../../../domain/usecases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const { _id, ...accountWithoutId } = await MongoHelper.insertIntoAndRetrieve('accounts', accountData)
+    return Object.assign({}, accountWithoutId, { id: _id })
+  }
+}
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -9,5 +9,24 @@ export const MongoHelper = {
 
   async disconnect(): Promise<void> {
     await this.client.close()
+  },
+
+  getCollection(name: string): Collection {
+    return this.client.db().collection(name)
+  },
+
+  async insertInto(collectionName: string, data: Object) {
+    const collection = this.getCollection(collectionName)
+    return await collection.insertOne(data)
+  },
+
+  async findById(collectionName: string, id: number) {
+    const collection = this.getCollection(collectionName)
+    return await collection.findOne(id)
+  },
+
+  async insertIntoAndRetrieve(collectionName: string, data: Object) {
+    const { insertedId } = await this.insertInto(collectionName, data)
+    return await this.findById(collectionName, insertedId)
   }
 }
