fb3b82458c0d4951287677d8fed70c0c825318f9
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-05-19 21:22:03 -0300

    test: move required field validation to ValidationComposite

---
+++ src/main/factories/signup-validation.spec.ts
@@ -0,0 +1,19 @@
+import { RequiredFildValidation } from '../../presentation/helpers/validators/required-field-validation'
+import { Validation } from '../../presentation/helpers/validators/validation'
+import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
+import { makeSignUpValidation } from './signup-validation'
+
+jest.mock('../../presentation/helpers/validators/validation-composite')
+
+describe('SignUpValidation Factory', () => {
+  test('Call ValidationComposite with all validations', () => {
+    makeSignUpValidation()
+    const validations: Validation[] = []
+
+    for (const field of ['name', 'email', 'password', 'password_confirmation']) {
+      validations.push(new RequiredFildValidation(field))
+    }
+
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
