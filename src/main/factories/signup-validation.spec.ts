5358754a127c984b3cb2bf83c48507da61ca1723
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-05-24 17:04:16 -0300

    style: remove typo from validator name

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -1,6 +1,6 @@
 import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation'
 import { EmailValidation } from '../../presentation/helpers/validators/email-validation'
-import { RequiredFildValidation } from '../../presentation/helpers/validators/required-field-validation'
+import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
 import { Validation } from '../../presentation/helpers/validators/validation'
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
 import { EmailValidator } from '../../presentation/protocols/email-validator'
@@ -23,7 +23,7 @@ describe('SignUpValidation Factory', () => {
     const validations: Validation[] = []
 
     for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
-      validations.push(new RequiredFildValidation(field))
+      validations.push(new RequiredFieldValidation(field))
     }
 
     validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
--- src/main/factories/signup-validation.ts
+++ src/main/factories/signup-validation.ts
@@ -1,7 +1,7 @@
 import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation'
 import { EmailValidation } from '../../presentation/helpers/validators/email-validation'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
-import { RequiredFildValidation } from '../../presentation/helpers/validators/required-field-validation'
+import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
 import { Validation } from '../../presentation/helpers/validators/validation'
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
 
@@ -9,7 +9,7 @@ export const makeSignUpValidation = (): ValidationComposite => {
   const validations: Validation[] = []
 
   for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
-    validations.push(new RequiredFildValidation(field))
+    validations.push(new RequiredFieldValidation(field))
   }
 
   validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
