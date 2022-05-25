7766fc1db8361d456111df22b5f3ed61e54b5fb2
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-05-25 18:08:01 -0300

    refactor: move data layer interfaces to folders

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -4,7 +4,7 @@ import {
   HttpResponse
 } from '../../presentation/protocols'
 import { LogControllerDecorator } from './log'
-import { LogErrorRepository } from '../../data/protocols/log-error-repository'
+import { LogErrorRepository } from '../../data/protocols/db/log-error-repository'
 import { serverError, ok } from '../../presentation/helpers/http/http-helper'
 import { AccountModel } from '../../domain/models/account'
 
