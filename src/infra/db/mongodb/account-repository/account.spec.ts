9f46c45fb17e88ac65f5b5727c1802038ff4d13d
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-05-26 16:02:16 -0300

    test: ensure AccountMongoRepository updates the account accessToken on updateAccessToken success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -56,4 +56,18 @@ describe('Account Mongo Repository', () => {
     const account = await sut.loadByEmail('any_email@mail.com')
     expect(account).toBeFalsy()
   })
+
+  test('Update the account accessToken on updateAccessToken success', async () => {
+    const sut = makeSut()
+    const account = await MongoHelper.insertIntoAndRetrieve('accounts', {
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    expect(account.accessToken).toBeFalsy()
+    await sut.updateAccessToken(account._id, 'any_token')
+    const updatedAccount = await MongoHelper.findById('accounts', account._id)
+    expect(updatedAccount).toBeTruthy()
+    expect(updatedAccount.accessToken).toBeTruthy()
+  })
 })
