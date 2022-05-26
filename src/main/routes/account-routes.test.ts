36beb9b0b717215d973a094169387e4318cd0d65
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-05-26 17:15:44 -0300

    refactor: rename signup routes with account routes

--- src/main/routes/account-routes.test.ts
+++ src/main/routes/account-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
-describe('SignUp Routes', () => {
+describe('Account Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -16,14 +16,16 @@ describe('SignUp Routes', () => {
     await accountCollection.deleteMany({})
   })
 
-  test('Return an account on success', async () => {
-    const response = await request(app).post('/api/signup').send({
-      name: 'Fabian',
-      email: 'gabriel.fabian@gmail.com',
-      password: '123',
-      passwordConfirmation: '123'
-    })
+  describe('POST /signup', () => {
+    test('Return 200 on signup', async () => {
+      const response = await request(app).post('/api/signup').send({
+        name: 'Fabian',
+        email: 'gabriel.fabian@gmail.com',
+        password: '123',
+        passwordConfirmation: '123'
+      })
 
-    expect(response.status).toBe(200)
+      expect(response.status).toBe(200)
+    })
   })
 })
