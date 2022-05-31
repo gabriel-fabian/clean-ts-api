4f3b12c81f59aebd64805ff73981d6df8ed064f2
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-05-31 17:44:32 -0300

    test: ensure add survey route returns 204 if valid access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,8 +2,11 @@ import { Collection } from 'mongodb'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
 let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
@@ -15,8 +18,10 @@ describe('Survey Routes', () => {
   })
 
   beforeEach(async () => {
-    surveyCollection = MongoHelper.getCollection('accounts')
+    surveyCollection = MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+    accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   describe('POST /surveys', () => {
@@ -34,5 +39,39 @@ describe('Survey Routes', () => {
 
       expect(response.status).toBe(403)
     })
+  })
+
+  test('Return 204 on add survey with valid accessToken', async () => {
+    const { _id: id } = await MongoHelper.insertIntoAndRetrieve('accounts', {
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+      role: 'admin'
+    })
+    const accessToken = sign({ id }, env.jwtSecret)
+
+    await accountCollection.updateOne({
+      _id: id
+    }, {
+      $set: {
+        accessToken: accessToken
+      }
+    })
+
+    const response = await request(app)
+      .post('/api/surveys')
+      .set('x-access-token', accessToken)
+      .send({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        }, {
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        }]
+      })
+
+    expect(response.status).toBe(204)
   })
 })
