e4251befa6d1d293d45fe16d6350bb07f2243e6c
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-05-31 17:31:30 -0300

    feat: ensure add survey route returns 403 if no access token is provided

--- src/main/factories/controllers/account/login/login-controller-factory.ts
+++ src/main/factories/controllers/account/login/login-controller-factory.ts
@@ -1,7 +1,7 @@
 import { Controller } from '../../../../../presentation/protocols'
 import { LoginController } from '../../../../../presentation/controllers/account/login/login-controller'
 import { makeLoginValidation } from './login-validation-factory'
-import { makeDbAuthentication } from '../../../usecases/authentication/db-authentication-factory'
+import { makeDbAuthentication } from '../../../usecases/account/authentication/db-authentication-factory'
 import { makeLogControllerDecorator } from '../../../decorators/log-controller-decoretor-factory'
 
 export const makeLoginController = (): Controller => {
--- src/main/factories/controllers/account/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/account/signup/signup-controller-factory.ts
@@ -1,6 +1,6 @@
 import { Controller } from '../../../../../presentation/protocols'
 import { makeDbAddAccount } from '../../../usecases/account/add-account/db-add-account-factory'
-import { makeDbAuthentication } from '../../../usecases/authentication/db-authentication-factory'
+import { makeDbAuthentication } from '../../../usecases/account/authentication/db-authentication-factory'
 import { makeSignUpValidation } from './signup-validation-factory'
 import { SignUpController } from '../../../../../presentation/controllers/account/signup/signup-controller'
 import { makeLogControllerDecorator } from '../../../decorators/log-controller-decoretor-factory'
