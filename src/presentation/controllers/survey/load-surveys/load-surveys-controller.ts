c5b3c0e5a10a695afe5b2e57cc6f047489450216
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-05-31 23:18:11 -0300

    feat: ensure LoadSurveysController calls LoadSurveysUseCase

---
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -0,0 +1,12 @@
+import { HttpRequest, HttpResponse, Controller, LoadSurveys } from './load-surveys-controller-protocols'
+
+export class LoadSurveysController implements Controller {
+  constructor (
+    private readonly loadSurveys: LoadSurveys
+  ) {}
+
+  async handle(_httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.loadSurveys.load()
+    return null
+  }
+}
