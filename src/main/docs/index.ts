bab6941e4a4a17f2993d016ffe38e57805e4b168
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-06-12 18:43:09 -0300

    chore: release 0.7.0

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -7,7 +7,7 @@ export default {
   info: {
     title: 'Clean Node Api',
     description: 'API para realizar enquetes entre programadores',
-    version: '0.6.0'
+    version: '0.7.0'
   },
   license: {
     name: 'GPL-3.0-or-later',
