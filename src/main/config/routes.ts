0e5bebd58db1f0d2ff7a45573cd1256bdc8d73d0
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-06-15 20:55:07 -0300

    feat: redirect root path to api-docs

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -4,6 +4,9 @@ import path from 'path'
 
 export default (app: Express): void => {
   const router = Router()
+  app.get('/', (_req, res) => {
+    res.redirect('/api-docs')
+  })
   app.use('/api', router)
   readdirSync(path.join(__dirname, '..', 'routes')).map(async file => {
     if (!file.includes('.test.') && !file.endsWith('.map')) {
